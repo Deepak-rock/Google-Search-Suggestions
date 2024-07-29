620a8feeb58dfd2e586ec116f9a7af5dadcb91e8
Deepak-rock <deepakind777@gmail.com>
2024-07-29 16:39:13 +0530

    second commit

--- src/components/GoogleSuggestions/index.js
+++ src/components/GoogleSuggestions/index.js
@@ -1,1 +1,63 @@
 // Write your code here
+import {Component} from 'react'
+import SuggestionItem from '../SuggestionItem/index'
+
+import './index.css'
+
+class GoogleSuggestions extends Component {
+  state = {searchInput: ''}
+
+  onChangeSearchInput = event => {
+    this.setState({searchInput: event.target.value})
+  }
+
+  updateSuggestion = value => {
+    this.setState({searchInput: value})
+  }
+
+  render() {
+    const {searchInput} = this.state
+    const {suggestionsList} = this.props
+    const searchResult = suggestionsList.filter(eachSuggestion =>
+      eachSuggestion.suggestion
+        .toLowerCase()
+        .includes(searchInput.toLowerCase()),
+    )
+
+    return (
+      <div className="app-container">
+        <img
+          className="google-logo"
+          src="https://assets.ccbp.in/frontend/react-js/google-logo.png"
+          alt="google logo"
+        />
+        <div className="input-container">
+          <div className="search-input-container">
+            <img
+              className="search-icon"
+              alt="search icon"
+              src="https://assets.ccbp.in/frontend/react-js/google-search-icon.png "
+            />
+            <input
+              type="search"
+              className="input"
+              value={searchInput}
+              placeholder="Search Google"
+              onChange={this.onChangeSearchInput}
+            />
+          </div>
+          <ul className="Suggestion-list">
+            {searchResult.map(eachitem => (
+              <SuggestionItem
+                key={eachitem.id}
+                suggestionsDetails={eachitem}
+                updateSuggestion={this.updateSuggestion}
+              />
+            ))}
+          </ul>
+        </div>
+      </div>
+    )
+  }
+}
+export default GoogleSuggestions
--- src/components/SuggestionItem/index.js
+++ src/components/SuggestionItem/index.js
@@ -1,1 +1,25 @@
 // Write your code here
+import './index.css'
+
+const SuggestionItem = props => {
+  const {suggestionsDetails, updateSuggestion} = props
+  const {suggestion} = suggestionsDetails
+
+  const onClickSuggestion = () => {
+    updateSuggestion(suggestion)
+  }
+
+  return (
+    <li className="suggestion-item">
+      <p className="suggestion">{suggestion}</p>
+      <button className="arrow" type="button" onClick={onClickSuggestion}>
+        <img
+          className="arrow-icon"
+          src="https://assets.ccbp.in/frontend/react-js/diagonal-arrow-left-up.png "
+          alt="arrow"
+        />
+      </button>
+    </li>
+  )
+}
+export default SuggestionItem
